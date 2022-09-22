f0d139530a9fe1ccf3b91822d4d845bd9d40e6a3
Daniel Rodrigues <drodrigues@moonpay.com>
2022-09-22 22:16:12 +0200

    Boilerplate register page

--- webapp/pages/register/index.tsx
+++ webapp/pages/register/index.tsx
@@ -1,20 +1,40 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import styles from "./register.module.css";
+import styles from "./register.module.scss";
 
 const Register: NextPage = () => {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Register</title>
-        <meta name="description" content="Monedero App register page." />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <div className={styles.background}>
+      <div className={styles.wrapper}>
+        <div className={styles.formContainer}>
+          <Head>
+            <title>Register</title>
+            <meta name="description" content="Monedero App register page." />
+            <link rel="icon" href="/favicon.ico" />
+          </Head>
+          <main className={styles.main}>
+            <h1 className={styles.title}>Sign up</h1>
+            <p className={styles.description}>
+              Join us and don't let your wallet manage your life anymore!
+            </p>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>Register</h1>
-        <p className={styles.description}>Coming soon!</p>
-      </main>
+            <form>
+              <input
+                className={styles.input}
+                type="email"
+                name="email"
+                placeholder="Email Address"
+              />
+              <input
+                className={styles.input}
+                type="password"
+                name="password"
+                placeholder="Password"
+              />
+            </form>
+          </main>
+        </div>
+      </div>
     </div>
   );
 };
